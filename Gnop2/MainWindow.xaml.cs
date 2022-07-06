562c7e38da225133c1c862fc28679356ba6e9f3a
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2022-07-06 10:13:37 +0200

    implemented paddle collision with ball and removed collsion with gamearea on x axis

--- Gnop2/MainWindow.xaml.cs
+++ Gnop2/MainWindow.xaml.cs
@@ -21,11 +21,8 @@ namespace Gnop2
         private bool PLDown = false;
         private bool PRUp = false;
         private bool PRDown = false;
-
-        // TODO: Aufgabe
-        /*
-         * Bauen Sie eine Collision-Detection ein, um den Ball an den "Paddles" abprallen zu lassen.
-         */
+        private int PLScore = 0;
+        private int PRScore = 0;
         public MainWindow()
         {
             InitializeComponent();
@@ -42,20 +39,20 @@ namespace Gnop2
             double prY = Canvas.GetTop(RightPaddle);
             // Left Player Movement
             if (PLUp)
-                if (plY > 10)
-                    Canvas.SetTop(LeftPaddle, plY - 10);
+                if (plY > paddleVelocity)
+                    Canvas.SetTop(LeftPaddle, plY - paddleVelocity);
                 else Canvas.SetTop(LeftPaddle, 0);
             else if (PLDown)
                 if (plY + LeftPaddle.ActualHeight < GameArea.ActualHeight)
-                    Canvas.SetTop(LeftPaddle, plY + 10); 
+                    Canvas.SetTop(LeftPaddle, plY + paddleVelocity); 
             // Right Player Movement
             if (PRUp)
-                if (prY > 10)
-                    Canvas.SetTop(RightPaddle, prY - 10);
+                if (prY > paddleVelocity)
+                    Canvas.SetTop(RightPaddle, prY - paddleVelocity);
                 else Canvas.SetTop(RightPaddle, 0);
             else if (PRDown)
                 if (prY + RightPaddle.ActualHeight < GameArea.ActualHeight)
-                    Canvas.SetTop(RightPaddle, prY + 10);
+                    Canvas.SetTop(RightPaddle, prY + paddleVelocity);
         }
 
         private void _animateBall(object? sender, EventArgs e)
@@ -64,14 +61,37 @@ namespace Gnop2
             double x = Canvas.GetLeft(Ball);
             double y = Canvas.GetTop(Ball);
 
-            #region directionX
             // move ball on x
             if (directionRight) Canvas.SetLeft(Ball, x + ballVelocity);
             else Canvas.SetLeft(Ball, x - ballVelocity);
 
-            // check if ball is outside x boundary area
-            if (x >= GameArea.ActualWidth - Ball.ActualWidth) directionRight = false;
-            else if (x <= 0) directionRight = true;
+            // check if ball is outside x upper boundary of gamearea
+            if (x >= GameArea.ActualWidth - Ball.ActualWidth)
+            {
+                PLScore += 1;
+                Init();
+            }
+            // ball is outside lower boundary of gamearea
+            else if (x <= 0)
+            {
+                PRScore += 1;
+                Init();
+            }
+            #region paddle collision detection
+            // left paddle collision detection
+            if (x <= LeftPaddle.ActualWidth &&
+                y >= Canvas.GetTop(LeftPaddle) &&
+                y + Ball.ActualHeight <= Canvas.GetTop(LeftPaddle) + LeftPaddle.ActualHeight)
+            {
+                directionRight = true;
+            }
+            // right paddle collision detection
+            if (x + Ball.ActualWidth >= Canvas.GetLeft(RightPaddle) &&
+                y >= Canvas.GetTop(RightPaddle) &&
+                y + Ball.ActualHeight <= Canvas.GetTop(RightPaddle) + RightPaddle.ActualHeight)
+            {
+                directionRight = false;
+            }
             #endregion
 
             #region directionY
@@ -109,10 +129,12 @@ namespace Gnop2
             // set position of left paddle
             Canvas.SetLeft(LeftPaddle, 0);
             Canvas.SetTop(LeftPaddle, (GameArea.ActualHeight - LeftPaddle.ActualHeight) / 2);
-
             // set position of ball
             Canvas.SetLeft(Ball, (GameArea.ActualWidth - Ball.ActualWidth) / 2);
             Canvas.SetTop(Ball, (GameArea.ActualHeight - Ball.ActualHeight) / 2);
+            // set scoreboard
+            Txbl_P1Score.Text = PLScore.ToString();
+            Txbl_P2Score.Text = PRScore.ToString();
         }
 
         #region KeyboardEvents for Movement
